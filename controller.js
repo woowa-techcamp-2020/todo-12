da54000e6eae5f2930c7a5c5957b718ba8b3abd8
eunjung <jenny.it.kr@gmail.com>
2020-07-14 16:06:27 +0900

    Add POST /users route for test
    - user 생성 기능 추가

--- controller.js
+++ controller.js
@@ -1,12 +1,38 @@
 const User = require("./user.model.js");
 
+// Create and Save a new Customer
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+  
+  // Create a Customer
+  const user = new User({
+    name: req.body.name,
+  });
+  
+  // Save Customer in the database
+  User.create(user, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the User."
+      });
+    else res.send(data);
+  });
+};
+
+
 // Retrieve all Customers from the database.
 exports.findAll = (req, res) => {
   User.getAll((err, data) => {
     if (err)
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving customers."
+          err.message || "Some error occurred while retrieving users."
       });
     else res.send(data);
   });
--- user.model.js
+++ user.model.js
@@ -5,8 +5,21 @@ const User = function(user) {
   this.name = user.name;
 };
 
+User.create = (newUser, result) => {
+  sql.query("INSERT INTO user SET ?", newUser, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created user: ", { id: res.insertId, ...newUser });
+    result(null, { id: res.insertId, ...newUser });
+  });
+};
+
 User.getAll = result => {
-  sql.query("SELECT * FROM USER", (err, res) => {
+  sql.query("SELECT * FROM user", (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(null, err);

c151366f82fad2ea85c58150fc9b969f09cd13fd
eunjung <jenny.it.kr@gmail.com>
2020-07-14 10:28:06 +0900

    Add db configuration and Create connection
    - .env 파일 생성 후 해당 파일 내에 DB_HOST, DB_USER, DB_DB, DB_PW 설정한 뒤 사용

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+ 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DB,
+  password: process.env.DB_PW
+});
+
+connection.connect(error => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("Successfully connected to the database.");
+  }
+});
+
+module.exports = connection;

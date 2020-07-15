b8f1ef330888195093b4d15d1a3451d10b0cd425
jungcome7 <jungcome7@gmail.com>
2020-07-15 19:28:20 +0900

    Make Mysql connection

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
 const port = 3000;
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DB,
+    password: process.env.DB_PW
+})
+
+connection.connect(err => {
+    if(err) console.error(err);
+
+    console.log('Successfully connected to the database.');
+})
 
 app.get('/', (req, res) => res.send('hihi'));
 

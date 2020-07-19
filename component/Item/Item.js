51045eb61269268828e54d6135ade48b01b0e795
eunjung <jenny.it.kr@gmail.com>
2020-07-19 12:56:22 +0900

    Add item update div
    - 기존 아이템 컴포넌트 더블클릭시 수정화면으로 전환 구현

--- component/Item/Item.js
+++ component/Item/Item.js
@@ -11,20 +11,34 @@ export default class {
     const item = document.createElement("div");
     item.classList.add("item");
     item.innerHTML = `
-      <div class="item__col">logo</div>
-      <div class="item__col">
-        <span class="item__content">${this.content}</span>
-        <span class="item__creator">Added by ${this.performer_username}</span>
+      <div class="item__show">
+        <div class="item__col">logo</div>
+        <div class="item__col">
+          <span class="item__content">${this.content}</span>
+          <span class="item__creator">Added by ${this.performer_username}</span>
+        </div>
+        <div class="item__col"><span class="item__close-btn">X</span></div>
       </div>
-      <div class="item__col"><span class="item__close-btn">X</span></div>`;
+      <div class="item__update hide">
+        <textarea></textarea>
+        <div class="item__update-btns">
+          <button class="update-btn">Update</button>
+          <button class="cancel-btn">Cancel</button>
+        </div>
+      </div>
+      `;
+
     item.addEventListener("dblclick", this.handleDoubleClick);
     const closeBtn = item.querySelector(".item__close-btn");
     closeBtn.addEventListener("click", this.preDelete);
     return item;
   }
 
-  handleDoubleClick() {
-    console.log(`item "${this.content}" : double clicked`);
+  handleDoubleClick({ currentTarget: item }) {
+    const showingDiv = item.querySelector(".item__show");
+    const updateDiv = item.querySelector(".item__update");
+    showingDiv.classList.add("hide");
+    updateDiv.classList.remove("hide");
   }
 
   fetchUpdate() {}

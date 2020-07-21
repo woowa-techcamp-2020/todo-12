3a12304b73c1fc9c1521766e3bacd63957aab264
eunjung <jenny.it.kr@gmail.com>
2020-07-21 11:49:01 +0900

    Add users fetch
    - User 클래스의 constructor, render 메서드 구현
    - 최초 페이지 접속시 유저 목록 fetch 받아 선택할 수 있게끔 렌더링 구현
    - 관련 스타일 수정

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import "./scss/styles.scss";
 
+import User from "../component/User/User.js";
 import List from "../component/List/List.js";
 
 const listCreationBtn = document.querySelector(".listCreationBtn");
@@ -18,6 +19,20 @@ const handleListCreationBtnClick = function () {
   document.body.appendChild(listNode);
 };
 
+const fetchUser = function () {
+  fetch(`http://localhost:3000/users`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((userData) => {
+        const user = new User(userData);
+        const userNode = user.render();
+        const container = document.querySelector(".collection");
+        container.appendChild(userNode);
+      });
+    });
+};
+
 window.addEventListener("DOMContentLoaded", () => {
+  fetchUser();
   listCreationBtn.addEventListener("click", handleListCreationBtnClick);
 });

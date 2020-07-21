d3cb060ef229bad276351fefe3c3f0a2fc55849e
eunjung <jenny.it.kr@gmail.com>
2020-07-21 10:26:24 +0900

    Add data parser
    - 쿼리문의 결과값으로 받은 데이터를 트리 구조로 파싱하여 클라이언트로 전달하는 기능 구현

---
+++ parser.js
@@ -0,0 +1,36 @@
+exports.parser = (data) => {
+  if (data.length === 0) return;
+  result = {};
+  let lastList = {};
+  result.board_id = data[0]["board_id"];
+  result.board_name = data[0]["board_name"];
+  result.lists = [];
+
+  for (let idx = 0; idx < data.length; idx++) {
+    if (lastList.list_id !== data[idx]["list_id"]) {
+      if (Object.keys(lastList).length > 0) {
+        result.lists.push(lastList);
+        lastList = {};
+      }
+
+      lastList = {
+        list_id: data[idx]["list_id"],
+        list_title: data[idx]["list_title"],
+        list_position: data[idx]["list_position"],
+        items: [],
+      };
+    }
+
+    const item = {
+      item_id: data[idx]["item_id"],
+      item_content: data[idx]["item_content"],
+      item_position_in_list: data[idx]["item_position_in_list"],
+      item_performer_name: data[idx]["item_performer_name"],
+    };
+
+    lastList.items.push(item);
+    result.lists.push(lastList);
+  }
+  // console.log(result);
+  return result;
+};

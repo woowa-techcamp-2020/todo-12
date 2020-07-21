8599b9ae4b0b7dee6894b574bbc411be5a02ea85
jungcome7 <jungcome7@gmail.com>
2020-07-22 00:17:02 +0900

    Make 'Item Drop' Feature in a column
    - 한 개의 칼럼 내부에서 드랍 기능 동작 구현

--- utils/drag-drop/drag-drop.js
+++ utils/drag-drop/drag-drop.js
@@ -6,8 +6,12 @@ export default function (event, droppableElementClass) {
     originMouseX,
     originMouseY;
 
+  const ITEM_MARGIN_TOP = 8; 
+  const ITEM_HEIGHT = 100;
+  const ITEM_WIDTH = 330;
+
   const setInitialCoordinates = function () {
-    originTop = selectedElem.offsetTop;
+    originTop = selectedElem.offsetTop - ITEM_MARGIN_TOP;
     originLeft = selectedElem.offsetLeft;
     originMouseX = event.pageX;
     originMouseY = event.pageY;
@@ -27,7 +31,7 @@ export default function (event, droppableElementClass) {
     draggableItem.classList.add("dragging");
     draggableItem.style.top = originTop + "px";
     draggableItem.style.left = originLeft + "px";
-    document.body.appendChild(draggableItem);
+    selectedElem.parentElement.appendChild(draggableItem);
   };
 
   const leaveSelectedItem = function () {
@@ -46,7 +50,45 @@ export default function (event, droppableElementClass) {
     const diffY = e.pageY - originMouseY;
     elem.style.top = originTop + diffY + "px";
     elem.style.left = originLeft + diffX + "px";
+
+    const draggableItemCenter = {
+      x: draggableItem.getBoundingClientRect().x + (draggableItem.getBoundingClientRect().width / 2),
+      y: draggableItem.getBoundingClientRect().y + (draggableItem.getBoundingClientRect().height / 2)
+    }
+
+    draggableItem.classList.add("hide")
+
+
+
+
+    const el = document.elementFromPoint(draggableItemCenter.x, draggableItemCenter.y).closest('.item');
+    
+
+    // console.log([...selectedElem.parentElement.childNodes].filter(v => v === selectedElem));
+    // console.log([...selectedElem.parentElement.childNodes].indexOf([...selectedElem.parentElement.childNodes].find(v => v === selectedElem)))
+    const itemsArr = [...selectedElem.parentElement.childNodes];
+    const selectedElemIdx = itemsArr.indexOf(selectedElem);
+    const elIdx = itemsArr.indexOf(el);
+    console.log('el: ', elIdx);
+    console.log('sel: ', selectedElemIdx)
+
+    if(el) {
+      if(selectedElemIdx > elIdx) {
+        selectedElem.insertAdjacentElement('afterend', el)
+      } else if(selectedElemIdx < elIdx) {
+        selectedElem.insertAdjacentElement('beforebegin', el);
+      } else if(el < 0) {
+        console.log('err')
+      }
+    }
+  
+    draggableItem.classList.remove("hide")
   };
+
+// 1. item position (data-position)
+// 2. direction : up / down
+// 3. 
+
 
   const initDragNDropOnMouseDown = function () {
     selectedElem = event.currentTarget;
@@ -56,6 +98,5 @@ export default function (event, droppableElementClass) {
     draggableItem.addEventListener("mousemove", handleMouseMove);
     draggableItem.addEventListener("mouseup", handleMouseUp);
   };
-
   initDragNDropOnMouseDown();
 }

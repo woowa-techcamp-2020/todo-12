93a6c792c12d3fda190d10d19600b421daff764b
eunjung <jenny.it.kr@gmail.com>
2020-07-23 02:41:32 +0900

    Create UserSelection/Selected within UserWrapper
    - UserSelection 은 유저를 선택하기 전에 선택 가능한 전체 유저를 보여주는 영역 ( + 유저 생성 )
    - UserSelected 는 유저를 선택한 후에 선택된 유저를 보여주는 영역 ( + 뒤로 가기 )

--- client/component/UserWrapper/UserWrapper.js
+++ client/component/UserWrapper/UserWrapper.js
@@ -1,7 +1,14 @@
+import UserSelection from "../UserSelection/UserSelection.js";
+import UserSelected from "../UserSelected/UserSelected.js";
+
 export default class UserWrapper {
   constructor({ target }) {
     const userWrapper = document.createElement("div");
     userWrapper.className = "user-wrapper";
     target.appendChild(userWrapper);
+
+    this.userWrapper = userWrapper;
+    this.userSelection = new UserSelection({ target: userWrapper });
+    this.userSelected = new UserSelected({ target: userWrapper });
   }
 }

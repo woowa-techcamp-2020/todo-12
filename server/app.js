c0262581f85717ac8fb6aa6a5ba518d34fa45b88
eunjung <jenny.it.kr@gmail.com>
2020-07-22 13:37:10 +0900

    Organize router
    - 하나의 문지기 라우터로 users, boards, lists, items 라우터를 묶어주었음

--- server/app.js
+++ server/app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const userRouter = require("./routes/users.js");
-const boardRouter = require("./routes/boards.js");
-const listRouter = require("./routes/lists.js");
-const itemRouter = require("./routes/items.js");
 
 const app = express();
 const PORT = 3000;
+
+const router = require("./routes");
 
 const setHeader = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -20,11 +18,6 @@ app.use(setHeader);
 app.use(logger("dev"));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("hello"));
-
-app.use("/users", userRouter);
-app.use("/boards", boardRouter);
-app.use("/lists", listRouter);
-app.use("/items", itemRouter);
+app.use("/", router);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));

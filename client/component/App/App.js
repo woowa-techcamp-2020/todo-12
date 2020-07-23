0fb223aba19d4a9546a38673e25779e78e812bc1
eunjung <jenny.it.kr@gmail.com>
2020-07-23 11:01:45 +0900

    Add Main setState with fetchBoard
    - App 에서 boardId 상태가 변경되었을 때 Main 에 board 정보를 fetch 받아 입력

--- client/component/App/App.js
+++ client/component/App/App.js
@@ -4,7 +4,15 @@ import Main from "./Main/Main.js";
 export default class App {
   constructor(target) {
     this.app = target;
+    this.boardId = null;
     this.header = new Header({ target: app });
     this.main = new Main({ target: app });
+
+    this.setState(1);
+  }
+
+  setState(selectedBoardId) {
+    this.boardId = selectedBoardId;
+    this.main.setState(selectedBoardId);
   }
 }
--- client/component/App/Main/Main.js
+++ client/component/App/Main/Main.js
@@ -1,11 +1,25 @@
 import Board from "./Board/Board.js";
+import { api } from "../../../api.js";
 
 export default class Main {
   constructor({ target }) {
     const main = document.createElement("main");
     target.appendChild(main);
-
     this.main = main;
+    this.data = null;
     this.board = new Board({ target: main });
+  }
+
+  setState(boardId) {
+    this.fetchBoard(boardId);
+    // this.render()
+  }
+
+  async fetchBoard(boardId) {
+    try {
+      await api.get.board(boardId).then((data) => {
+        this.data = data;
+      });
+    } catch {}
   }
 }
